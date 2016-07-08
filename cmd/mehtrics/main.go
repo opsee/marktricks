51b4e4224ecd3d003d5b7c288bb183c0aad71170
Dan Compton <dan@opsee.com>
2016-07-08 15:46:15 -0400

    what

---
+++ cmd/mehtrics/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	builder "github.com/ajityagaty/go-kairosdb/builder"
+	client "github.com/ajityagaty/go-kairosdb/client"
+	"github.com/gogo/protobuf/proto"
+	_ "github.com/lib/pq"
+	"github.com/nsqio/go-nsq"
+	"github.com/opsee/basic/schema"
+	log "github.com/opsee/logrus"
+	"github.com/opsee/mehtrics/worker"
+	"github.com/spf13/viper"
+)
+
+func init() {}
+
+func main() {
+	viper.SetEnvPrefix("mehtrics")
+	viper.AutomaticEnv()
+
+	viper.SetDefault("log_level", "info")
+	logLevelStr := viper.GetString("log_level")
+	logLevel, err := log.ParseLevel(logLevelStr)
+	if err != nil {
+		log.WithError(err).Error("Could not parse log level, using default.")
+		logLevel = log.InfoLevel
+	}
+	log.SetLevel(logLevel)
+
+	nsqConfig := nsq.NewConfig()
+	nsqConfig.MaxInFlight = 4
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	maxTasks := viper.GetInt("max_tasks")
+	// in-memory cache of customerId -> bastionId
+	consumer, err := worker.NewConsumer(&worker.ConsumerConfig{
+		Topic:            "_.results",
+		Channel:          "kairosdb-test",
+		LookupdAddresses: viper.GetStringSlice("nsqlookupd_addrs"),
+		NSQConfig:        nsqConfig,
+		HandlerCount:     maxTasks,
+	})
+
+	if err != nil {
+		log.WithError(err).Fatal("Failed to create consumer.")
+	}
+
+	cli := client.NewHttpClient("http://172.30.200.227:8080")
+	consumer.AddHandler(func(msg *nsq.Message) error {
+		result := &schema.CheckResult{}
+		if err := proto.Unmarshal(msg.Body, result); err != nil {
+			log.WithError(err).Error("Error unmarshalling message from NSQ.")
+			return err
+		}
+
+		logger := log.WithFields(log.Fields{
+			"customer_id": result.CustomerId,
+			"check_id":    result.CheckId,
+			"bastion_id":  result.BastionId,
+		})
+
+		if result.CustomerId == "" || result.CheckId == "" {
+			logger.Error("Received invalid check result.")
+			return nil
+		}
+
+		mb := builder.NewMetricBuilder()
+		for _, resp := range result.Responses {
+			logger.Debugf("check reply type: %T", resp.Reply)
+			switch t := resp.Reply.(type) {
+			case *schema.CheckResponse_HttpResponse:
+				for _, m := range t.HttpResponse.Metrics {
+					switch m.Name {
+					case "request_latency":
+						mb.AddMetric("request_latency").
+							AddDataPoint(result.Timestamp.Millis(), m.Value).
+							AddTag("check", result.CheckId).
+							AddTag("customer", result.CustomerId)
+					default:
+						logger.Debugf("unsupported metric type: %s", m.Name)
+					}
+				}
+			default:
+				logger.Debugf("unsupported check type: %s", t)
+			}
+		}
+		pushResp, err := cli.PushMetrics(mb)
+		logger.Debugf("pushresp: %s, err: %v", pushResp, err)
+
+		return nil
+	})
+
+	if err := consumer.Start(); err != nil {
+		log.WithError(err).Fatal("Failed to start consumer.")
+	}
+
+	<-sigChan
+
+	consumer.Stop()
+}
