aa0780bc192f095ded51dcf9137f8d32cb9e4fef
Dan Compton <dan@opsee.com>
2016-07-12 16:38:13 -0400

    cats -> mehtrics

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ APPENV=${APPENV:-mehtricsenv}
 /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$APPENV > /$APPENV
 
  source /$APPENV && \
- /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$CATS_CERT > /$MEHTRICS_CERT && \
-  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$CATS_CERT_KEY > /$MEHTRICS_CERT_KEY && \
+ /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$MEHTRICS_CERT > /$MEHTRICS_CERT && \
+  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$MEHTRICS_CERT_KEY > /$MEHTRICS_CERT_KEY && \
     chmod 600 /$MEHTRICS_CERT_KEY && \
 	/worker
