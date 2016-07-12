2cd13a4288f6309cfbf93bc849f55adf165ebe83
Dan Compton <dan@opsee.com>
2016-07-12 16:38:56 -0400

    cleanup

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ APPENV=${APPENV:-mehtricsenv}
 
 /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$APPENV > /$APPENV
 
- source /$APPENV && \
- /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$MEHTRICS_CERT > /$MEHTRICS_CERT && \
-  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$MEHTRICS_CERT_KEY > /$MEHTRICS_CERT_KEY && \
+source /$APPENV && \
+    /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$MEHTRICS_CERT > /$MEHTRICS_CERT && \
+    /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$MEHTRICS_CERT_KEY > /$MEHTRICS_CERT_KEY && \
     chmod 600 /$MEHTRICS_CERT_KEY && \
 	/worker
